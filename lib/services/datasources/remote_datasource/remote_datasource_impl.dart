0fc0b8b8b94f24ef31cfcde5112af110db23cac3
Danyial Khan <danyialkhan667@gmail.com>
2022-10-15 11:34:57 +0500

    Update remote_datasource_impl.dart

--- lib/services/datasources/remote_datasource/remote_datasource_impl.dart
+++ lib/services/datasources/remote_datasource/remote_datasource_impl.dart
@@ -1,5 +1,14 @@
+import 'package:logger/logger.dart';
+import 'package:muhammad_danyial_tentwenty_assignment/services/datasources/remote_datasource/dio_wrapper.dart';
 import 'package:muhammad_danyial_tentwenty_assignment/services/datasources/remote_datasource/remote_data_source.dart';
 
 class RemoteDataSourceImpl extends RemoteDataSource {
+  final DioWrapper _dioWrapper;
+  final Logger _logger;
 
+  RemoteDataSourceImpl({
+    required DioWrapper dioWrapper,
+    required Logger logger,
+  })  : _dioWrapper = dioWrapper,
+        _logger = logger;
 }
