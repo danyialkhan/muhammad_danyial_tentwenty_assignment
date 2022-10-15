7fad19c89c2e6ee70c87090db32e99d2a482baef
Danyial Khan <danyialkhan667@gmail.com>
2022-10-15 17:05:02 +0500

    created usecase for search movie

--- lib/app/api_paths/api_paths.dart
+++ lib/app/api_paths/api_paths.dart
@@ -2,4 +2,5 @@ class APIPaths {
   static String onGetUpComingMovies = "movie/upcoming";
   static String onGetMovieDetails = "movie/";
   static String onGetGenre = "genre/movie/list";
+  static String onSearchMovie = "search/movie";
 }
